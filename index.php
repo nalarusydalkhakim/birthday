1086bc18222c3cf5cc678b267a893860c94b4344
Nala AlHakim <nalarusydalkhakim@gmail.com>
2022-06-11 20:13:50 +0700

    Edit text 2

---
+++ index.php
@@ -0,0 +1,157 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>Happy Birthday!!! :)</title>
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
+  <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400" rel="stylesheet" />
+  <link rel="shortcut icon" type="image/png" href="img/favicon.png" />
+  <link rel="stylesheet" href="styles/main.css" />
+</head>
+
+<body>
+
+  <audio class="song" loop autoplay>
+    <source src="./music/hbd.mpeg">
+    </source>
+    Your browser isn't invited for super fun audio time.
+  </audio>
+
+  <div class="container">
+    <div class="one">
+      <h1 class="one">
+        Hi
+        <span>Alni</span>
+      </h1>
+      <p class="two" >Untuk perempuan yang mewarnai duniaku <i class="fa fa-heart" aria-hidden="true"></i> </p>
+    </div>
+
+    <div class="three">
+      <p>It's your birthday!! :D</p>
+    </div>
+
+    <div class="four">
+      <div class="text-box">
+        <p class="hbd-chatbox">
+          Happy birthday to youu Sayangkuuuu !!, Sayang aku gak tau mau ngomong apa :") yang pasti semoga kamu diberikan kesehatan jadi jangan banyak-banyak minum ESSSSS T_T, dan semoga kamu diberi rezeki yang buanyakkk dan berkah, Semoga apa yang kamu inginkan tercapai, semoga jadi manusia yang selalu beruntung dalam hal apapun. dan semoga apa lagi ya? duh bingung aku :p ....... POKOKNYA SEMOGA YANG TERSEMOGAKAN DIKABULKAN OLEH ALLAH SWT.... AAMIIN YA ALLAH :) </p>
+        <p class="fake-btn">Send</p>
+      </div>
+    </div>
+
+    <div class="five">
+      <p class="idea-1">My Wish For you...</p>
+      <p class="idea-2">Selalu jadi orang baik ya sayangku, entah kepada siapapun itu <i class="fa fa-heart" aria-hidden="true"></i></p>
+      <p class="idea-3">
+        btw, aku tau kenapa kamu begitu<br>
+        <strong>spesial</strong>
+        .
+      </p>
+      <p class="idea-4">Karena.....</p>
+      <p class="idea-5">
+        aku juga gak tau sayang T_T,tiba-tiba jadi orang spesial aja kamu nih
+        <span>:)</span>
+      </p>
+      <p class="idea-6">
+        <span>S</span>
+        <span>O</span>
+      </p>
+    </div>
+
+    <div class="six">
+      <img src="./img/alni.jpg" alt="profile" class="profile-picture" id="imagePath"/>
+      <img src="img/hat.svg" alt="hat" class="hat" />
+      <div class="wish">
+        <h3 class="wish-hbd">Happy Birthday!</h3>
+        <h5 >Semua yang terbaik semoga senantiasa bersamamu ;)</h5>
+      </div>
+    </div>
+
+    <div class="seven">
+      <div class="baloons">
+        <img src="img/ballon2.svg" alt="" />
+        <img src="img/ballon1.svg" alt="" />
+        <img src="img/ballon3.svg" alt="" />
+        <img src="img/ballon1.svg" alt="" />
+        <img src="img/ballon2.svg" alt="" />
+        <img src="img/ballon3.svg" alt="" />
+        <img src="img/ballon2.svg" alt="" />
+        <img src="img/ballon3.svg" alt="" />
+        <img src="img/ballon1.svg" alt="" />
+        <img src="img/ballon2.svg" alt="" />
+        <img src="img/ballon3.svg" alt="" />
+        <img src="img/ballon2.svg" alt="" />
+        <img src="img/ballon1.svg" alt="" />
+        <img src="img/ballon3.svg" alt="" />
+        <img src="img/ballon2.svg" alt="" />
+        <img src="img/ballon3.svg" alt="" />
+        <img src="img/ballon1.svg" alt="" />
+        <img src="img/ballon2.svg" alt="" />
+        <img src="img/ballon1.svg" alt="" />
+        <img src="img/ballon3.svg" alt="" />
+        <img src="img/ballon3.svg" alt="" />
+        <img src="img/ballon1.svg" alt="" />
+        <img src="img/ballon2.svg" alt="" />
+        <img src="img/ballon3.svg" alt="" />
+        <img src="img/ballon2.svg" alt="" />
+        <img src="img/ballon1.svg" alt="" />
+        <img src="img/ballon3.svg" alt="" />
+        <img src="img/ballon2.svg" alt="" />
+        <img src="img/ballon3.svg" alt="" />
+        <img src="img/ballon1.svg" alt="" />
+        <img src="img/ballon2.svg" alt="" />
+        <img src="img/ballon1.svg" alt="" />
+        <img src="img/ballon3.svg" alt="" />
+      </div>
+    </div>
+
+    <div class="eight">
+      <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="20" cy="20" r="20" />
+      </svg>
+      <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="20" cy="20" r="20" />
+      </svg>
+      <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="20" cy="20" r="20" />
+      </svg>
+      <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="20" cy="20" r="20" />
+      </svg>
+      <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="20" cy="20" r="20" />
+      </svg>
+      <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="20" cy="20" r="20" />
+      </svg>
+      <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="20" cy="20" r="20" />
+      </svg>
+      <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="20" cy="20" r="20" />
+      </svg>
+      <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="20" cy="20" r="20" />
+      </svg>
+    </div>
+
+    <div class="nine">
+      <p>Maaf ya sayang, aku belum bisa ke tangerang, sebenernya aku pengen banget.. :")</p>
+      <p id="replay">Semoga saja aku bisa segera kesana ketemu kamu.</p>
+      <p class="last-smile">:)</p>
+    </div>
+  </div>
+
+</body>
+  <!-- Greensock -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js"></script>
+  <!-- Sweetalert -->
+  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <script type="application/javascript" src="script/main.js"></script>
+
+</html>
